ce65068bda07f40d10875dec91461b1cbbd7e73e
Javier Leyva Seiglie <96433186+jseiglie@users.noreply.github.com>
2022-09-19 19:25:53 +0200

    files uploaded

---
+++ server/index.js
@@ -0,0 +1,62 @@
+const { Router } = require("express");
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const app = express();
+
+const FoodModel = require("./Models/Food");
+
+app.use(express.json());
+app.use(cors());
+mongoose.connect(
+  "mongodb+srv://jseiglie:zyUasFGpbo0sX75J@crud.zap8mps.mongodb.net/food?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+  }
+);
+
+app.post("/insert", async (req, res) => {
+  const { foodName, daysSinceIEat } = req.body;
+  const food = new FoodModel({
+    foodName: foodName,
+    daysSinceIEat: daysSinceIEat,
+  });
+
+  try {
+    food.save();
+  } catch (error) {
+    console.log(error);
+  }
+  res.send("inserted data");
+});
+
+app.get("/read", async (req, res) => {
+  //FoodModel.find({ $where: {foodName: "apple"}})
+  FoodModel.find({}, (err, result) => {
+    if (err) {
+      res.send(err);
+    }
+    res.send(result);
+  });
+});
+
+app.put("/update", async (req, res) => {
+  const { id, newFoodName } = req.body;
+  try {
+    await FoodModel.findByIdAndUpdate(id, {foodName: newFoodName})
+    res.send("updated")
+
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.delete("/delete/:id", async (req, res)=>{
+  const id = req.params.id
+  await FoodModel.findByIdAndDelete(id)
+})
+
+
+app.listen(3001, () => {
+  console.log("server up on 3001");
+});
